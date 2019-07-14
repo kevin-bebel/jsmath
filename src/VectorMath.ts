646f720013939d19341acee74e962e0b246c041f
Kevin Bebel <kevin@rapidtrade.biz>
2019-07-14 20:03:56 +0200

    clean up vector math module

--- src/VectorMath.ts
+++ src/VectorMath.ts
@@ -1,46 +1,59 @@
 import Vector from './Vector'
 
-export function VectorAdd(...vectors : Array<Vector> )  :  Vector {
-   if ( vectors.length === 0 ) {
-        throw Error("No argument passed in to the vector addition function"); 
-   }
+export default class VectorMath {
+    //This should be used for vector deduction. 
+    static VectorAdd(...vectors: Array<Vector>): Vector {
+        if (vectors.length === 0) {
+            throw Error("No argument passed in to the vector addition function");
+        }
 
-   //First we need to check that the vector are matching in dimension.
-   let size : number;
+        //First we need to check that the vector are matching in dimension.
+        const size = VectorMath.isValidVectors(vectors)
+        return VectorMath.operatorVector("addition", size, vectors);
+    }
 
-   for(let i = 0; i < vectors.length; i++){
-       if (i === 0) {
-           size  = vectors[i].size();
-           continue;
-       }
+     private static isValidVectors(vectors: Array<Vector>): number {
+        //First we need to check that the vector are matching in dimension.
+        let size: number;
+        for (let i = 0; i < vectors.length; i++) {
+            if (i === 0) {
+                size = vectors[i].size();
+                continue;
+            }
 
-       if(vectors[i].size() !== size) {
-           throw Error("Cannot add vectors of different dimensions")
-           break;
-       }
-   }
+            if (vectors[i].size() !== size) {
+                throw Error("Cannot add vectors of different dimensions")
+                break;
+            }
+        }
 
-   //Now for the vector addition
-   let count = 0;
+        return size;
+    }
 
-   let output_vector = new Vector();
-
-   while ( count < size ) {
-       //The sum of all the values at given index
-       let index_total = 0;
-       let _internal_count = 0; 
-
-       //This loop will allow us to add the all the components at the same level for the vector add istion
-       while ( _internal_count < vectors.length ) {
-            index_total = index_total + vectors[_internal_count].getValueAtIndex(count)
-            _internal_count = _internal_count + 1;
-       }
-
-       output_vector.add(index_total);
-       //we then push the index total into the Vector that we are return back. 
-
-       count = count + 1;
-   }
-   
-   return output_vector;
+    private static operatorVector(operator: string, size: number, vectors: Array<Vector>): Vector {
+        let count = 0;
+        let output_vector = new Vector();
+    
+        while (count < size) {
+            //The sum of all the values at given index
+            let index_total = 0;
+            let _internal_count = 0;
+            //This loop will allow us to add the all the components at the same level for the vector add istion
+            while (_internal_count < vectors.length) {
+                
+                if( operator === "addition"){
+                    index_total = index_total + vectors[_internal_count].getValueAtIndex(count)
+                } else {
+                    index_total = index_total - vectors[_internal_count].getValueAtIndex(count)
+                }
+                _internal_count = _internal_count + 1;
+            }
+    
+            output_vector.add(index_total);
+            //we then push the index total into the Vector that we are return back. 
+            count = count + 1;
+        }
+    
+        return output_vector;
+    }
 }
--- tests/VectorMath.test.ts
+++ tests/VectorMath.test.ts
@@ -1,14 +1,11 @@
 import {expect,assert} from 'chai';
 import Vector from '../src/Vector';
-import { VectorAdd } from '../src/VectorMath';
+import  VectorMath from '../src/VectorMath';
 
-describe('Vector Intialization', () => {
+describe('Basic vector addition', () => {
     it('Should add two vectors', () => {
       const v1 = new Vector(1,2,3,4,5);
       const v2 = new Vector(1,2,3,4,5);
-
-      const result = new Vector(2,4,6,8,10);
-
-      expect( VectorAdd(v1,v1).get()).to.eql([2,4,6,8,10]);
+      expect( VectorMath.VectorAdd(v1,v1).get()).to.eql([2,4,6,8,10]);
     }); 
 });
