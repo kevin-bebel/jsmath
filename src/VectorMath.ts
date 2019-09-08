e22d20c70514b47f5af811f4b0e6b2fd47bce4dc
Kevin Bebel <kevin@rapidtrade.biz>
2019-09-08 17:32:40 +0200

    math updates

--- src/VectorMath.ts
+++ src/VectorMath.ts
@@ -1,18 +1,20 @@
 import Vector from './Vector'
 
 export default class VectorMath {
-    //This should be used for vector deduction. 
     static VectorAdd(...vectors: Array<Vector>): Vector {
-        if (vectors.length === 0) {
-            throw Error("No argument passed in to the vector addition function");
-        }
-
+        if (vectors.length === 0) throw Error("No argument passed in to the vector addition function");
         //First we need to check that the vector are matching in dimension.
         const size = VectorMath.isValidVectors(vectors)
         return VectorMath.operatorVector("addition", size, vectors);
     }
+    static VectorSubtract(...vectors: Array<Vector>): Vector {
+        if (vectors.length === 0) throw Error("No argument passed in to the vector addition function");
+        //First we need to check that the vector are matching in dimension.
+        const size = VectorMath.isValidVectors(vectors)
+        return VectorMath.operatorVector("subtraction", size, vectors);
+    }
 
-     private static isValidVectors(vectors: Array<Vector>): number {
+    private static isValidVectors(vectors: Array<Vector>): number {
         //First we need to check that the vector are matching in dimension.
         let size: number;
         for (let i = 0; i < vectors.length; i++) {
@@ -33,27 +35,27 @@ export default class VectorMath {
     private static operatorVector(operator: string, size: number, vectors: Array<Vector>): Vector {
         let count = 0;
         let output_vector = new Vector();
-    
+
         while (count < size) {
             //The sum of all the values at given index
             let index_total = 0;
             let _internal_count = 0;
             //This loop will allow us to add the all the components at the same level for the vector add istion
             while (_internal_count < vectors.length) {
-                
-                if( operator === "addition"){
+                //I have to break
+                if (operator === "addition") {
                     index_total = index_total + vectors[_internal_count].getValueAtIndex(count)
                 } else {
                     index_total = index_total - vectors[_internal_count].getValueAtIndex(count)
                 }
                 _internal_count = _internal_count + 1;
             }
-    
+
             output_vector.add(index_total);
             //we then push the index total into the Vector that we are return back. 
             count = count + 1;
         }
-    
+
         return output_vector;
     }
 }
--- tests/VectorMath.test.ts
+++ tests/VectorMath.test.ts
@@ -1,11 +1,28 @@
-import {expect,assert} from 'chai';
+import { expect, assert } from 'chai';
 import Vector from '../src/Vector';
-import  VectorMath from '../src/VectorMath';
+import VectorMath from '../src/VectorMath';
 
-describe('Basic vector addition', () => {
-    it('Should add two vectors', () => {
-      const v1 = new Vector(1,2,3,4,5);
-      const v2 = new Vector(1,2,3,4,5);
-      expect( VectorMath.VectorAdd(v1,v1).get()).to.eql([2,4,6,8,10]);
-    }); 
+describe('Basic vector arithmetic operations', () => {
+  it('Should add two vectors', () => {
+    const v1 = new Vector(5, 4, 5, 6, 7, 7);
+    const v2 = new Vector(2, 4, 3, 6, 4, 2);
+
+    expect(VectorMath.VectorAdd(v1, v2).get()).to.eql([7, 8, 8, 12, 11, 9]);
+  });
+
+
+  it('Should subtract ( a - b )', () => {
+    const v1 = new Vector(5, 4, 5, 6, 7, 7);
+    const v2 = new Vector(2, 4, 3, 6, 4, 2);
+
+    expect(VectorMath.VectorSubtract(v1, v2).get()).to.eql([3, 0, 2, 0, 3, 5]);
+  });
+
+  it('Should subtract ( b -  a )', () => {
+    const v1 = new Vector(5, 4, 5, 6, 7, 7);
+    const v2 = new Vector(2, 4, 3, 6, 4, 2);
+
+    expect(VectorMath.VectorSubtract(v2, v1).get()).to.eql([-3, 0, -2, 0, -3, -5]);
+  });
+
 });
